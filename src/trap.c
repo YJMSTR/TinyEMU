313a14c8f6a0931f6e352d59ec350a4184387ca6
yjmstr <pyjmstr@gmail.com>
2023-05-20 00:36:47 +0800

    fix error in trap_handler

--- src/trap.c
+++ src/trap.c
@@ -8,7 +8,7 @@ void trap_handler(DECODER *decoder, enum TRAP traptype, bool isException, uint64
         return;
     }
     enum CPU_PRI_LEVEL nxt_level = M;
-    if (cpu.pri_level == S) {
+    if (cpu.pri_level <= S) {
         if ((isException && (get_csr(medeleg) & (1 << cause)))
             || (!isException && (get_csr(mideleg) & (1 << cause)))) {   
             nxt_level = S;
